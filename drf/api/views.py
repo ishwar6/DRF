2cd171de9b987501f8b5884569583bd5716e1773
ishwar6 <iisudrj26@gmail.com>
2022-01-12 18:06:25 +0530

    basic

--- drf/api/views.py
+++ drf/api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from .serializers import StudentSerilizer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Student
 
 
-def student_list(request, pk):
+def student_list_old(request, pk):
     s = Student.objects.get(id=pk)
   #  print(s)
     serializer = StudentSerilizer(s)
@@ -16,12 +16,20 @@ def student_list(request, pk):
 
 
 
+def student_list(request, pk):
+    s = Student.objects.get(id=pk)
+    serializer = StudentSerilizer(s)
+    return JsonResponse(serializer.data)
+
+
 def student_all(request):
     s = Student.objects.all()
   #  print(s)
     serializer = StudentSerilizer(s, many=True)
   #  print(serializer)
   #  print(dir(serializer))
-    json_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(json_data, content_type = "application/json")
+    #json_data = JSONRenderer().render(serializer.data)
+   # return HttpResponse(json_data, content_type = "application/json")
+    return JsonResponse(serializer.data, safe = False)
+
 
