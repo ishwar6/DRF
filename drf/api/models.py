c2d98c18c261db5cb6a79c5970ceb526b51a78c0
ishwar6 <iisudrj11@gmail.com>
2022-01-24 10:41:30 +0530

    done

--- drf/api/models.py
+++ drf/api/models.py
@@ -5,4 +5,7 @@ class Student(models.Model):
     name = models.CharField(max_length=11)
     roll = models.IntegerField()
     city = models.CharField(max_length=100)
-    
+
+
+class Notification(models.Model):
+    user = models.CharField(max_length=22)
